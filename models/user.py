d4ab7045ff2ce35688fc5db8ce7fc1fb03fbbf77
Raidenlea <86613539+Raidenlea@users.noreply.github.com>
2022-11-26 15:10:06 -0300

    Fix task 7

--- models/user.py
+++ models/user.py
@@ -12,5 +12,3 @@ class User(BaseModel, Base):
     password = Column(String(128), nullable=False)
     first_name = Column(String(128), nullable=True)
     last_name = Column(String(128), nullable=True)
-    places = relationship('Place', backref='users', cascade='delete')
-    reviews = relationship('Review', backref='users', cascade='delete')
