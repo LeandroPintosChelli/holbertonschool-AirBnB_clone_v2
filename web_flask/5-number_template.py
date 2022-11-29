8a138c9b0103a9baebe2ca19278e8b442f7e9766
Raidenlea <86613539+Raidenlea@users.noreply.github.com>
2022-11-29 15:02:17 -0300

    Fix task 5, function number_template

--- web_flask/5-number_template.py
+++ web_flask/5-number_template.py
@@ -38,7 +38,7 @@ def isInt(n):
 @app.route('/number_template/<int:n>', strict_slashes=False)
 def number_template(n):
     if type(n) == int:
-        return render_template('5-number.html', n=89)
+        return render_template('5-number.html', number=n)
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=5000)
