26c46f2534608b7b2bc6f0e26296c47f141aa0c2
Raidenlea <86613539+Raidenlea@users.noreply.github.com>
2022-11-29 16:16:29 -0300

    Add task 8

--- web_flask/8-cities_by_states.py
+++ web_flask/8-cities_by_states.py
@@ -7,8 +7,8 @@ app = Flask(__name__)
 
 
 @app.route('/states_list', strict_slashes=False)
-def states():
-    return render_template('7-states_list.html',
+def cities():
+    return render_template('8-cities_by_states.html',
                            states=storage.all('State').values())
 
 
