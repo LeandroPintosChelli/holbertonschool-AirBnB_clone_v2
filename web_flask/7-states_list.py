f878326e561408b7d7eb6df828ffc23ad7279122
Raidenlea <86613539+Raidenlea@users.noreply.github.com>
2022-11-29 16:08:17 -0300

    Restore task 7

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -1,20 +1,20 @@
 #!/usr/bin/python3
 """Starts a Flask web application"""
+from models import storage
 from flask import Flask, render_template
+from models import storage
+from models.state import State
 app = Flask(__name__)
 
 
 @app.route('/states_list', strict_slashes=False)
 def states():
-    from models import storage
-    from models.state import State
-    sts = storage.all(State)
-    return render_template("7-states_list.html", sts=sts)
+    store = storage.all(State)
+    return render_template("7-states_list.html", store=store)
 
 
 @app.teardown_appcontext
 def teardown(self):
-    from models import storage
     storage.close()
 
 if __name__ == "__main__":
