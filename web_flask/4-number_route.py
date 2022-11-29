6d7ef84c7ed93ba997c523647ae15a4698f9a070
Raidenlea <86613539+Raidenlea@users.noreply.github.com>
2022-11-29 14:39:43 -0300

    Add task 4

---
+++ web_flask/4-number_route.py
@@ -0,0 +1,37 @@
+#!/usr/bin/python3
+"""starts a Flask web application"""
+
+from flask import Flask
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def Holberton():
+    return "Hello HBNB!"
+
+
+@app.route('/hbnb', strict_slashes=False)
+def School():
+    return "HBNB"
+
+
+@app.route('/c/<text>', strict_slashes=False)
+def C_again(text):
+    rep = text.replace('_', ' ')
+    return 'C %s' % rep
+
+
+@app.route('/python', defaults={'text': 'is cool'}, strict_slashes=False)
+@app.route('/python/<text>', strict_slashes=False)
+def IsCool(text):
+    rep = text.replace('_', ' ')
+    return 'Python %s' % rep
+
+
+@app.route('/number/<n>', strict_slashes=False)
+def isInt(n):
+    if type(n) == int:
+        return '%i is a number' % n
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=5000)
