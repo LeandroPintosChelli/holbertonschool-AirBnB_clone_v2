3f01020b3d71c3cb22fd301be5dfb90c775f574b
Raidenlea <86613539+Raidenlea@users.noreply.github.com>
2022-12-02 20:18:47 +0000

    Fix db and file storage

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -63,5 +63,7 @@ class FileStorage:
             pass
 
     def close(self):
-        """Deserializes the JSON file to objects"""
+        """
+        Close method
+        """
         self.reload()
