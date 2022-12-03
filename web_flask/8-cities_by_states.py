d72ac5a5fd28106d37270fa58a878ed3c7745173
Raidenlea <86613539+Raidenlea@users.noreply.github.com>
2022-12-03 14:42:47 +0000

    Add task 9

--- web_flask/8-cities_by_states.py
+++ web_flask/8-cities_by_states.py
@@ -3,10 +3,11 @@
 from models import storage
 from flask import Flask, render_template
 from models.state import State
+
 app = Flask(__name__)
 
 
-@app.route('/states_list', strict_slashes=False)
+@app.route('/cities_by_states', strict_slashes=False)
 def cities():
     return render_template('8-cities_by_states.html',
                            states=storage.all('State').values())
@@ -15,6 +16,7 @@ def cities():
 @app.teardown_appcontext
 def teardown(self):
     storage.close()
+
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=5000)
